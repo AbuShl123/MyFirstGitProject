c96e4cf60a9a14ed255d0f2d881813a6250528de
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-08-17 12:03:56 +0500

    changing a value of 'int a', and creating first for loop.

--- src/EmptyClass.java
+++ src/EmptyClass.java
@@ -2,10 +2,14 @@ public class EmptyClass {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-        int a = 12;
+        int a = 10;
         System.out.println(a);
 
         String year = "20" + 12;
         System.out.println(year);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
